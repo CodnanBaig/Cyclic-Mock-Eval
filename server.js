a8b56c36135b570d443be7e544c35d8dcae86fb7
Adnan Baig <88790490+CodnanBaig@users.noreply.github.com>
2023-10-13 23:36:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const cors = require("cors");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(cors());
 server.use("", router);
 server.listen(process.env.PORT || 5000, () => {
   console.log("JSON Server is running");
